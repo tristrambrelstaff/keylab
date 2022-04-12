d28de94a40ade76d0b9581c56968e8eb86c01ff1
Tristram Brelstaff <tristrambrelstaff@gmail.com>
2022-04-12 18:44:54 +0100

    Added keys association to Document and document association Key. Updated views to include these.

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,2 +1,11 @@
 class Document < ApplicationRecord
+
+  has_many :keys
+
+  def Document.ref_id_pairs
+    Document.all.collect{|document|
+      [document.ref, document.id]
+    }.sort
+  end
+
 end
--- app/models/key.rb
+++ app/models/key.rb
@@ -1,2 +1,5 @@
 class Key < ApplicationRecord
+
+  belongs_to :document
+
 end
