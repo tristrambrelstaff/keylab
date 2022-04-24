04c5b532b00a4f12f9606a363ff6b5ac08555b9b
Tristram Brelstaff <tristrambrelstaff@gmail.com>
2022-04-24 20:35:53 +0100

    Switched database from Sqlite3 to Postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_04_10_185528) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "documents", force: :cascade do |t|
     t.string "ref"
     t.text "citation"
