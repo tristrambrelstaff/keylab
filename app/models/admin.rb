681e314e2ec3e43089e3500afac609e6068f5670
Tristram Brelstaff <tristrambrelstaff@gmail.com>
2022-05-18 13:06:19 +0100

    Now the only people who can sign up are those whose email address is listed in the KEYPAD_ADMIN_WHITELIST environment variable.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,20 @@
 class Admin < ApplicationRecord
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable, :omniauthable,
   # :recoverable, :rememberable, :validatable
   devise :database_authenticatable, :registerable
+
+  # Only people in the KEYPAD_ADMIN_WHITELIST environment variable are
+  # allowed to sign up.  This list consists of their email addresses
+  # separated by semicolons.
+  validates(
+    :email,
+    inclusion: {
+      in: ENV.fetch("KEYPAD_ADMIN_WHITELIST").split(";"),
+      message: "is not on the KEYPAD_ADMIN_WHITELIST"
+    }
+  )
+
+
 end
