bf9e2f91ade0f401df0f82a4eeb899af5f53466a
Tristram Brelstaff <tristrambrelstaff@gmail.com>
2022-09-05 22:01:25 +0100

    Migration to add predicates, properties and taxons tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_12_164400) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_05_214800) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -84,6 +84,23 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_12_164400) do
     t.integer "other_observation_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "predicates", force: :cascade do |t|
+    t.string "type"
+    t.integer "predicate_id"
+    t.integer "predicate_id_1"
+    t.integer "predicate_id_2"
+    t.integer "property_id"
+    t.integer "taxon_id"
+  end
+
+  create_table "properties", force: :cascade do |t|
+    t.text "text"
+  end
+
+  create_table "taxons", force: :cascade do |t|
+    t.text "text"
   end
 
 end
